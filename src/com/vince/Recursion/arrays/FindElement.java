fe3aa4fca53c5bad9db6751ef397bc83359ba8bb
Vincent Kashaka <vincekashaka@gmail.com>
2022-09-01 12:38:29 +0200

    added a findAllIndex method

--- src/com/vince/Recursion/arrays/FindElement.java
+++ src/com/vince/Recursion/arrays/FindElement.java
@@ -1,11 +1,16 @@
 package com.vince.Recursion.arrays;
 
+import java.util.ArrayList;
+
 public class FindElement {
 
     public static void main(String[] args) {
-        int[] arr = {2, 3, 1, 4, 5};
-        System.out.println(find(arr, 4, 0));
-        System.out.println(findIndex(arr, 4, 0));
+        int[] arr = {2, 3 ,1 ,4, 4, 5};
+//        System.out.println(find(arr, 4, 0));
+//        System.out.println(findIndex(arr, 4, 0));
+
+        ArrayList<Integer> ans = findAllIndex(arr, 4, 0, new ArrayList<>());
+        System.out.println(ans);
 
     }
 
@@ -25,5 +30,39 @@ public class FindElement {
         } else {
             return findIndex(arr, target, index + 1);
         }
+    }
+
+//    static ArrayList<Integer> list = new ArrayList<>();
+//    static void findAllIndex(int[]  arr, int target, int index) {
+//        if (index == arr.length) {
+//            return;
+//        }
+//        if (arr[index] == target) {
+//            list.add(index);
+//        }
+//            findAllIndex(arr, target, index + 1);
+//    }
+static ArrayList<Integer> findAllIndex(int[]  arr, int target, int index,  ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
+    }
+
+    static ArrayList<Integer> findAllIndex2(int[]  arr, int target, int index) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls =  findAllIndex2(arr, target, index + 1);
+        list.addAll(ansFromBelowCalls);
+
+        return list;
     }
 }
